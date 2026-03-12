f65ae9844d31258ea8207907729aecd0ec418445
matheus619silva <matheus619silva@gmail.com>
2026-03-11 21:23:31 -0300

    Primeira versão do simulador AZ-900

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{padding:40,fontFamily:"Arial"}}>
+      <h1>SIMULADO FUNCIONANDO 🚀</h1>
+      <p>Se você está vendo isso, o React atualizou corretamente.</p>
+    </div>
+  );
+}
